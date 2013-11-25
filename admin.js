547c9d5cf7fc0644d556892d9f794edf233a7e1e
BeOpenSource <beopensourcellc@gmail.com>
2013-11-25 13:49:22 -0500

    Update admin.js
    outline

--- admin.js
+++ admin.js
@@ -1,2 +1,12 @@
 // adminref.com admin.js main backbone app file
 
+var adminapp = new(Backbone.View.extend({
+  template: Mustache.compile(''),
+  start: function(){},
+  models: {},
+  views: {},
+  collections: {}
+  });
+  
+  adminapp.start();
+  
